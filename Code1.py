8214b6b19c101c31db113fad05ba6ca4e685ed6e
mayuka18 <mayuka.gajendragadkar2025@bitsdesign.edu.in>
2026-02-18 13:25:02 +0530

    Update Code1.py

--- Code1.py
+++ Code1.py
@@ -1,1 +1,33 @@
+from machine import Pin
+import time
+in1 = Pin(14, Pin.OUT)
+in2 = Pin(25, Pin.OUT)
+in3 = Pin(26, Pin.OUT)
+in4 = Pin(27, Pin.OUT)
+list = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+sec = 0.05
+
+
+while True :
+    for r in list :
+        in1.value(r[0])
+        
+        in2.value(r[1])
+        
+        in3.value(r[2])
+        
+        in4.value(r[3])
+        
+        time.sleep(sec)   
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
 
